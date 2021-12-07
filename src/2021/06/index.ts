5c9fbcb5df3afb85c645315865020be77224171e
nimbus117 <mail@nimbus117.co.uk>
2021-12-07 00:20:04 +0000

    2021-06

---
+++ src/2021/06/index.ts
@@ -0,0 +1,22 @@
+import { parseLineOfNumbers, range, sum } from '../../shared';
+
+const simulateGrowth = (fish: number[], days: number): number[] => {
+  const growth = fish.map((f) => (f > 0 ? f - 1 : [6, 8])).flat();
+  return days > 1 ? simulateGrowth(growth, days - 1) : growth;
+};
+
+const calculateGrowth = (fish: number[], days: number) => {
+  const count = range(0, 8).map((c) => fish.filter((f) => f === c).length);
+  return range(0, days - 1).reduce((acc) => {
+    const newFish = acc.shift() || 0;
+    acc[8] = newFish;
+    acc[6] += newFish;
+    return acc;
+  }, count);
+};
+
+export const part1 = (input: string) =>
+  simulateGrowth(parseLineOfNumbers(input), 80).length;
+
+export const part2 = (input: string) =>
+  sum(calculateGrowth(parseLineOfNumbers(input), 256));
