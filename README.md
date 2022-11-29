53c658e0473c58a4d009c1e539f3904987306827
nimbus117 <mail@nimbus117.co.uk>
2022-11-29 22:59:13 +0000

    add readme and refactor runner

---
+++ README.md
@@ -0,0 +1,13 @@
+Advent of Code
+==============
+
+[About Advent of Code](https://adventofcode.com/2022/about)
+
+Usage
+-----
+
+**npm start year day [part]**
+
+`npm start 2021 01`
+
+`npm start 2021 01 part1`
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import { readFileSync } from 'fs';
+import { readFile } from 'fs/promises';
 
-if (process.argv.length < 4) {
-  throw new Error('Expected year and day arguments (npm start 2021 01)');
-}
+if (process.argv.length < 4)
+  console.log('ERROR: Expected year and day arguments (npm start 2021 01)\n');
+else
+  (async () => {
+    const [, , year, day] = process.argv;
+    const parts = process.argv[4] ? [process.argv[4]] : ['part1', 'part2'];
+    const solution = await import(`./${year}/${day}`);
+    const input = await readFile(`./src/${year}/${day}/input`, 'utf8');
 
-const [, , year, day] = process.argv;
-const parts = process.argv[4] ? [process.argv[4]] : ['part1', 'part2'];
-const input = readFileSync(`./src/${year}/${day}/input`, 'utf8');
-
-(async () => {
-  const solution = await import(`./${year}/${day}`);
-
-  parts.forEach((part) => {
-    if (solution[part]) {
-      const start = new Date().valueOf();
-      console.log(`${part}:`, solution[part](input));
-      const end = new Date().valueOf();
-      console.log(`${end - start}ms\n`);
-    }
-  });
-})();
+    parts.forEach((part) => {
+      if (solution[part]) {
+        const start = new Date().valueOf();
+        console.log(`${part}:`, solution[part](input));
+        const end = new Date().valueOf();
+        console.log(`${end - start}ms\n`);
+      } else {
+        console.log(`ERROR: Could not find export named '${part}'\n`);
+      }
+    });
+  })();
