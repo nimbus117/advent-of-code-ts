cf150cc140305aae3706945ce527fbb63e0c6873
nimbus117 <mail@nimbus117.co.uk>
2022-12-08 09:17:38 +0000

    2022-07

---
+++ src/2022/07/fail.ts
@@ -0,0 +1,49 @@
+import { pipe } from '@shared/Function';
+import { parseLinesOfStrings } from '@shared/ParseInput';
+
+type File = number;
+type Dir = Map<string, Dir | File>;
+type GetDirTreeOut = { i: number; directory: Dir };
+
+const parseFile = (file: string) => {
+  const [size, name] = file.split(' ');
+  return {
+    size: parseInt(size),
+    name,
+  };
+};
+
+const getDirTree = (
+  lines: string[],
+  index = 0,
+  directory = new Map()
+): GetDirTreeOut => {
+  let i = index;
+  while (i < lines.length) {
+    const line = lines[i];
+    if (line.match(/^\$ cd/)) {
+      if (!line.match(/^\$ cd \.\./)) {
+        const tree = getDirTree(lines, i + 1, new Map());
+        directory.set(line.split(' ')[2], tree.directory);
+        i = tree.i + 1;
+        continue;
+      } else {
+        return { directory, i };
+      }
+    } else if (line.match(/^\d/)) {
+      const file = parseFile(line);
+      directory.set(file.name, file.size);
+    }
+    i++;
+  }
+
+  return { directory, i };
+};
+
+export const part1 = (input: string) =>
+  pipe(input)
+    ._(parseLinesOfStrings)
+    ._(getDirTree)
+    ._((x) => x.directory)
+    .__((x) => console.dir(x, { depth: 3 }))
+    .$();
---
+++ src/2022/07/index.ts
@@ -0,0 +1,45 @@
+import { filter, first, fromIterable, last, map, sort } from '@shared/Array';
+import { pipe } from '@shared/Function';
+import { MapWithDefault } from '@shared/Map';
+import { sum } from '@shared/Math';
+import { parseLinesOfStrings } from '@shared/ParseInput';
+
+function getFolderSizes(input: string) {
+  const dirs: string[] = [];
+  const dirMap = new MapWithDefault(0);
+
+  for (const line of parseLinesOfStrings(input)) {
+    if (line.startsWith('$ cd')) {
+      if (line.startsWith('$ cd ..')) dirs.pop();
+      else dirs.push(line.split(' ')[2]);
+    }
+
+    if (line.match(/^\d/)) {
+      const [size] = line.split(' ');
+      dirs.forEach((x, i) => {
+        const key = dirs.slice(0, i + 1).join('/');
+        dirMap.set(key, parseInt(size) + dirMap.get(key));
+      });
+    }
+  }
+
+  return pipe(dirMap)
+    ._(fromIterable)
+    ._(map((x) => x[1]))
+    .$();
+}
+
+export const part1 = (input: string) =>
+  pipe(input)
+    ._(getFolderSizes)
+    ._(filter((x) => x <= 100000))
+    ._(sum)
+    .$();
+
+export const part2 = (input: string) =>
+  pipe(input)
+    ._(getFolderSizes)
+    ._(sort((a, b) => a - b))
+    ._(filter((x, _, a) => x > 30000000 - (70000000 - last(a))))
+    ._(first)
+    .$();
