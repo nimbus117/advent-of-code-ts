a5685b90b5d5afecc58e1e99dfec3496a2ea21e5
nimbus117 <mail@nimbus117.co.uk>
2024-12-05 19:26:30 +0000

    2024-03

--- src/2024/02/index.ts
+++ src/2024/02/index.ts
@@ -1,4 +1,4 @@
-import { every, filter, map } from '@shared/Array';
+import { every, filter } from '@shared/Array';
 import { any, pipe } from '@shared/Function';
 import { count } from '@shared/Number';
 import { parseLinesOfStrings } from '@shared/ParseInput';
@@ -15,11 +15,7 @@ const isSafeWithDampner = (report: number[]) =>
   isSafe(report) || report.some((_, i) => isSafe(report.toSpliced(i, 1)));
 
 export const part1 = (input: string) =>
-  pipe(input)._(parseReport)._(map(isSafe))._(filter(Boolean))._(count).$;
+  pipe(input)._(parseReport)._(filter(isSafe))._(count).$;
 
 export const part2 = (input: string) =>
-  pipe(input)
-    ._(parseReport)
-    ._(map(isSafeWithDampner))
-    ._(filter(Boolean))
-    ._(count).$;
+  pipe(input)._(parseReport)._(filter(isSafeWithDampner))._(count).$;
